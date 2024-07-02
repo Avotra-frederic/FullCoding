166b71ff143daab4bbee7293d2a1f85de3d06cdb
Avotra-frederic <fred.avotra@gmail.com>
2024-07-02 16:31:17 +0300

    My first commit: update SEO update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -8,6 +8,26 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'yearly',
     },
     {
+      url: 'https://full-conding.vercel.app/#about',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+    },
+    {
+      url: 'https://full-conding.vercel.app/#services',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+    },
+    {
+      url: 'https://full-conding.vercel.app/#pricing',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+    },
+    {
+      url: 'https://full-conding.vercel.app/#contact',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+    },
+    {
       url: 'https://jssi.jssi-madagascar.mg/',
       lastModified: new Date(),
       changeFrequency: 'monthly',
