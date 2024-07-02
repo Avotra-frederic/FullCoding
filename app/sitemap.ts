c554e9967b7cc2f249305224dfa72f0463e454d5
Avotra-frederic <fred.avotra@gmail.com>
2024-07-02 16:14:04 +0300

    My first commit: update SEO update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -8,5 +8,17 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'yearly',
       priority: 1,
     },
+    {
+      url: 'https://jssi.jssi-madagascar.mg/',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 2,
+    },
+    {
+      url: 'https://espic.jssi-madagascar.mg/',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 2,
+    },
   ]
 }
