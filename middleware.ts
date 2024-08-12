9f85617df3039d779974495de68b5d09a6af21c4
Avotra-frederic <fred.avotra@gmail.com>
2024-08-12 12:44:24 +0300

    My first commit: add middleware

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,10 @@
+import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
 export function  middleware(req: NextRequest)
 {
-    const ip = req.headers.get("x-forwarded-for")?.toString() || req.ip;
-    console.log("User IP", ip)
+    const ip : any = req.headers.get("x-forwarded-for")?.toString() || req.ip;
+    const cookieStore = cookies();
+    cookieStore.set({name:"ip",value:ip});
     return NextResponse.next();
 }
